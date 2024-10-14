01e3bc50d1261c0d4b57a94ac14395465e61d9c0
loki20203 <151372360+loki20203@users.noreply.github.com>
2024-10-14 15:18:47 +0300

    Task_4

---
+++ Task_4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MyApp.Models;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть логін: ");
+        string login = Console.ReadLine();
+
+        Console.Write("Введіть ім'я: ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("Введіть прізвище: ");
+        string lastName = Console.ReadLine();
+
+        Console.Write("Введіть вік: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        User user = new User(login, firstName, lastName, age);
+
+        Console.WriteLine("\nІнформація про користувача:");
+        user.DisplayInfo();
+    }
+}
